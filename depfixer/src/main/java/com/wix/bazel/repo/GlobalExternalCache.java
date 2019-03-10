3c66e9909b4121436f4f9a2965d2739c192e921f
Shachar Anchelovich <shacharan@wix.com>
2019-03-10 17:04:50 +0200

    Use labeldex new bulk API  (#1589)
    * Use labeldex new bulk API #pr
    
    * Fix Optional import
    
    * Don't change classes without targets
    
    GitOrigin-RevId: 842f0d5ea5b405a23ca3b15e0b101a1d59ee4422

--- depfixer/src/main/java/com/wix/bazel/repo/GlobalExternalCache.java
+++ depfixer/src/main/java/com/wix/bazel/repo/GlobalExternalCache.java
@@ -3,22 +3,25 @@ package com.wix.bazel.repo;
 import com.wix.bazel.process.RunWithRetries;
 import com.wix.bazel.runmode.RunMode;
 import com.wixpress.ci.labeldex.domain.SymbolType;
+import com.wixpress.ci.labeldex.model.Bulk2ndPartyLabels;
+import com.wixpress.ci.labeldex.model.Bulk3rdPartyLabels;
 import com.wixpress.ci.labeldex.model.BulkLabels;
+import com.wixpress.ci.labeldex.model.BulkLabelsSocialMode;
 import com.wixpress.ci.labeldex.model.BulkSymbols;
-import com.wixpress.ci.labeldex.model.Label;
 import com.wixpress.ci.labeldex.model.SymbolLabels;
-import com.wixpress.ci.labeldex.model.BazelLabels;
+import com.wixpress.ci.labeldex.model.Symbol2ndPartyLabels;
+import com.wixpress.ci.labeldex.model.Symbol3rdPartyLabels;
 import com.wixpress.ci.labeldex.model.SecondPartyLabel;
 import com.wixpress.ci.labeldex.model.ThirdPartyLabel;
+import com.wixpress.ci.labeldex.model.Label;
 import com.wixpress.ci.labeldex.restclient.LabeldexRestClient;
-import com.wixpress.ci.labeldex.domain.SymbolType.*;
 import com.wixpress.ci.rest.client.RestClient;
-import scala.Option;
+import scala.Enumeration;
 import scala.collection.JavaConverters;
+import scala.collection.mutable.Buffer;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -44,39 +47,93 @@ public class GlobalExternalCache {
             return cache;
         }
 
-        if (runMode == RunMode.ISOLATED) {
-            getSingleLabelsAndAddToCache(filteredClasses);
-        } else {
-            getBulkLabelsAndAddToCache(filteredClasses);
-        }
+        getBulkLabelsAndAddToCache(filteredClasses);
 
         return cache;
     }
 
     private void getBulkLabelsAndAddToCache(List<String> classes) {
         BulkSymbols symbols = new BulkSymbols(JavaConverters.asScalaBuffer(classes).toSet());
-        BulkLabels clientLabels =
-                RunWithRetries.run(5, 500L, () -> labeldexrestClient.findBulkLabels(symbols));
-        List<SymbolLabels> symbolLabels = JavaConverters.bufferAsJavaList(clientLabels.symbolDocument().toBuffer());
+        BulkLabelsSocialMode clientLabels =
+                RunWithRetries.run(5, 500L,
+                        () -> labeldexrestClient.findSocialBulkLabels(symbols, srcWorkspaceName,
+                                runMode != RunMode.ISOLATED));
 
-        symbolLabels.stream().filter(l -> l.symbol().symbolType() != SymbolType.PACKAGE()).forEach(s -> {
-            Supplier<Stream<Label>> labelsStreamSupplier = () -> JavaConverters.setAsJavaSet(s.labels()).stream()
-                    .filter(l -> !l.workspace().equals(srcWorkspaceName));
+        if (clientLabels == null) {
+            return;
+        }
 
-            Supplier<Stream<Label>> fwLabelsStreamSupplier = () ->
-                    labelsStreamSupplier.get().filter(l -> l.workspace().equals("wix_platform_wix_framework"));
+        BulkLabels bazelLabels = clientLabels.bazelLabels();
+        Bulk2ndPartyLabels bulk2ndPartyLabels = clientLabels.secondPartyLabels();
+        Bulk3rdPartyLabels bulk3rdPartyLabels = clientLabels.thirdPartyLabels();
 
-            Stream<Label> labelsStream;
+        List<SymbolLabels> symbolLabels = filterPackages(bazelLabels.symbolDocument().toBuffer(),
+                s -> s.symbol().symbolType());
 
-            if (fwLabelsStreamSupplier.get().findAny().isPresent()) {
-                labelsStream = fwLabelsStreamSupplier.get();
-            } else {
-                labelsStream = labelsStreamSupplier.get();
-            }
+        List<Symbol2ndPartyLabels> symbol2ndPartyLabels = filterPackages(bulk2ndPartyLabels.symbolDocument().toBuffer(),
+                s -> s.symbol().symbolType());
 
-            labelsStream.forEach(l -> cache.put(s.symbol().fullyQualifiedName(),
-                    String.format("@%s%s", l.workspace(), fixLabel(l.label()))));
-        });
+        List<Symbol3rdPartyLabels> symbol3rdPartyLabels = filterPackages(bulk3rdPartyLabels.symbolDocument().toBuffer(),
+                s -> s.symbol().symbolType());
+
+        Map<String, Set<String>> bazelLabelsMap = toLabelsMap(symbolLabels,
+                s -> s.symbol().fullyQualifiedName(),
+                s -> symbolLabels(s).map(this::formatLabel).collect(Collectors.toSet()));
+
+        Map<String, Set<String>> symbol2ndPartyLabelsMap = toLabelsMap(symbol2ndPartyLabels,
+                s -> s.symbol().fullyQualifiedName(),
+                s -> JavaConverters.setAsJavaSet(s.labels()).stream()
+                        .map(SecondPartyLabel::label).collect(Collectors.toSet()));
+
+        Map<String, Set<String>> symbol3rdPartyLabelsMap = toLabelsMap(symbol3rdPartyLabels,
+                s -> s.symbol().fullyQualifiedName(),
+                s -> JavaConverters.setAsJavaSet(s.labels()).stream()
+                        .map(ThirdPartyLabel::label).collect(Collectors.toSet()));
+
+        for (String fqn : classes) {
+            Optional<Set<String>> maybeLabels =
+                    Stream.of(bazelLabelsMap, symbol3rdPartyLabelsMap, symbol2ndPartyLabelsMap)
+                    .map(x -> x.get(fqn))
+                    .filter(Objects::nonNull)
+                    .findFirst();
+
+            maybeLabels.ifPresent(strings -> strings.forEach(l -> cache.put(fqn, l)));
+        }
+    }
+
+    private Stream<Label> symbolLabels(SymbolLabels labels) {
+        Supplier<Stream<Label>> labelsStreamSupplier = () -> JavaConverters.setAsJavaSet(labels.labels()).stream()
+                .filter(l -> !l.workspace().equals(srcWorkspaceName));
+
+        Supplier<Stream<Label>> fwLabelsStreamSupplier = () ->
+                labelsStreamSupplier.get().filter(l -> l.workspace().equals("wix_platform_wix_framework"));
+
+        Stream<Label> labelsStream;
+
+        if (fwLabelsStreamSupplier.get().findAny().isPresent()) {
+            labelsStream = fwLabelsStreamSupplier.get();
+        } else {
+            labelsStream = labelsStreamSupplier.get();
+        }
+
+        return labelsStream;
+    }
+
+    private String formatLabel(Label l) {
+        return String.format("@%s%s", l.workspace(), fixLabel(l.label()));
+    }
+
+    private <S> List<S> filterPackages(Buffer<S> symbols, Function<S, Enumeration.Value> symbolTypeAccessor) {
+        List<S> symbolsList = JavaConverters.bufferAsJavaList(symbols.toBuffer());
+        return  symbolsList.stream()
+                .filter(x -> symbolTypeAccessor.apply(x) != SymbolType.PACKAGE())
+                .collect(Collectors.toList());
+    }
+
+    private <S> Map<String, Set<String>> toLabelsMap(List<S> symbols, Function<S, String> fqnAccessor,
+                                                     Function<S, Set<String>> labelsAccessor) {
+        return symbols.stream()
+                .collect(Collectors.toMap(fqnAccessor, labelsAccessor));
     }
 
     private String fixLabel(String label) {
@@ -88,34 +145,6 @@ public class GlobalExternalCache {
         return labelParts.length == 2 ?
                 String.format("%s:%s", labelPackage, labelName) :
                 labelPackage;
-    }
-
-    private void getSingleLabelsAndAddToCache(List<String> classes) {
-        classes.forEach(className -> {
-            BazelLabels labels = RunWithRetries.run(
-                    5,
-                    500L,
-                    () -> labeldexrestClient.findLabels(className, Option.apply(srcWorkspaceName), false));
-            Set<String> labelsToAdd = new HashSet<>();
-            if (labels.thirdPartyLabels().nonEmpty()) {
-                labelsToAdd = getThirdPartyLabels(labels.thirdPartyLabels());
-            } else if (labels.secondPartyLabels().nonEmpty()) {
-                labelsToAdd = getSecondPartyLabels(labels.secondPartyLabels());
-            }
-            labelsToAdd.forEach(label -> cache.put(className, label));
-        });
-    }
-
-    private Set<String> getSecondPartyLabels(scala.collection.immutable.Set<SecondPartyLabel> secondPartyLabels) {
-        return JavaConverters.setAsJavaSet(secondPartyLabels).stream()
-                .map(SecondPartyLabel::label)
-                .collect(Collectors.toSet());
-    }
-
-    private Set<String> getThirdPartyLabels(scala.collection.immutable.Set<ThirdPartyLabel> thirdPartyLabels) {
-        return JavaConverters.setAsJavaSet(thirdPartyLabels).stream()
-                .map(ThirdPartyLabel::label)
-                .collect(Collectors.toSet());
     }
 }
 
