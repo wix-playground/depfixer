caec5b33d2ef935593f4276d019c2016c9820722
Shachar Anchelovich <shacharan@wix.com>
2019-06-16 11:33:29 +0300

    Handle 'error: not found: value xml' (#2155)
    GitOrigin-RevId: 5dfc523f11ea30f6bdb6277009974752f87efb2e

--- depfixer/src/main/java/com/wix/bazel/analyze/ScalaXmlAnalyzer.java
+++ depfixer/src/main/java/com/wix/bazel/analyze/ScalaXmlAnalyzer.java
@@ -11,7 +11,8 @@ public class ScalaXmlAnalyzer extends AbstractTargetAnalyzer {
     public AnalyzerResult analyze(BrokenTargetData targetData) {
         AnalyzerResult result = new AnalyzerResult();
 
-        if (targetData.getStream().contains("scala.xml package must be on the classpath"))
+        if (targetData.getStream().contains("scala.xml package must be on the classpath") ||
+                targetData.getStream().contains("error: not found: value xml"))
             result.addClass("scala.xml", 100);
 
         return result;
